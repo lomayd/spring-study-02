a64cb238884c11ce10de21e929c7308255772c95
lomayd <jinho10167@gmail.com>
2023-01-14 22:24:51 +0900

    feat: Project Initialize

---
+++ src/main/java/lomayd/springstudy02/SpringStudy02Application.java
@@ -0,0 +1,13 @@
+package lomayd.springstudy02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudy02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudy02Application.class, args);
+	}
+
+}
