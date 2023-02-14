6ca2ae22cd7b4dbada6c89593f34a39376a59715
lomayd <jinho10167@gmail.com>
2023-02-14 23:40:33 +0900

    feat: App Config Spring Apply

--- src/main/java/lomayd/springstudy02/domain/AppConfig.java
+++ src/main/java/lomayd/springstudy02/domain/AppConfig.java
@@ -8,20 +8,28 @@ import lomayd.springstudy02.domain.member.service.MemberService;
 import lomayd.springstudy02.domain.member.service.MemberServiceImpl;
 import lomayd.springstudy02.domain.order.service.OrderService;
 import lomayd.springstudy02.domain.order.service.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
